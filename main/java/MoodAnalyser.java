29012768475850f90d72e963513b8a13921c74d4
Dipesh Maywade <dipeshmaywade@gmail.com>
2021-02-23 18:02:32 +0530

    [Deep]Refactore: MoodAnalyser.java Refactered For Making Constructore

--- main/java/MoodAnalyser.java
+++ main/java/MoodAnalyser.java
@@ -1,13 +1,19 @@
-
 public class MoodAnalyser {
 
     public String message;
 
-    public String analyseMood(String message){
-        if (message.contains("Sad")) {
-            return "SAD";
-        }
-        else {
+    public MoodAnalyser(String message) {
+        this.message = message;
+    }
+
+    public String analyseMood() {
+        try {
+            if (message.contains("Sad")) {
+                return "SAD";
+            } else {
+                return "HAPPY";
+            }
+        }catch (NullPointerException e){
             return "HAPPY";
         }
     }
